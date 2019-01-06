d601d31a86bd3109df84169a82e5d869586655f1
SWostik Shrestha <swostik.s.shrestha@utu.fi>
2019-01-06 20:15:43 +0200

    Updates

---
+++ precache-manifest.8e9c53c06772294a120dc36ab6879687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "066240afe6591d6b5127",
+    "url": "/2018-interactive-web-apps-project-Swostik/static/js/runtime~main.066240af.js"
+  },
+  {
+    "revision": "ae134804c344a0dfee77",
+    "url": "/2018-interactive-web-apps-project-Swostik/static/js/main.ae134804.chunk.js"
+  },
+  {
+    "revision": "43b1394e8f4e6db07523",
+    "url": "/2018-interactive-web-apps-project-Swostik/static/js/1.43b1394e.chunk.js"
+  },
+  {
+    "revision": "ae134804c344a0dfee77",
+    "url": "/2018-interactive-web-apps-project-Swostik/static/css/main.7e47fdd2.chunk.css"
+  },
+  {
+    "revision": "a31b90bd53e1c3e0e68cdde9ee9578be",
+    "url": "/2018-interactive-web-apps-project-Swostik/index.html"
+  }
+];
